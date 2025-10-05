a9329995d64b617e4f1f3a7c0a5d024b1c766695
raki-Code20 <rakeshtumuluri9553@gmail.com>
2025-10-05 21:11:33 +0530

    Added

---
+++ CALCULATOR2DARRAY.java
@@ -0,0 +1,17 @@
+public class Cal2DArray {
+    🧨🧨🧨🧨🧨 A 2Dimensional Array is an Array where each element is an Array.
+
+    public static void main(String[] args) {
+        char[][] numberPad1={{'1','2','3'},
+                {'4','5','6'},
+                {'7','8','9'},
+                {'','0','#'}};
+        for(char[] numberPad2  numberPad1){
+            for(char numberPad  numberPad2 ){
+                System.out.print(numberPad+ );
+            }
+            System.out.println( );
+        }
+
+    }
+}
